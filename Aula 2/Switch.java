a6f0e525196d2d2150fe04358c3fd91a8c0baf08
Giovanna <giovanna.regina2005@gmail.com>
2026-02-19 20:19:40 -0300

    Aula 2 arquivo incial

---
+++ Aula 2/Switch.java
@@ -0,0 +1,19 @@
+public class Switch{
+
+    public static void main(String[]args ){
+        int i = 10;
+        String s;
+         switch(i){
+            case 1:
+                s = "Um";
+                break;
+
+            default:
+                s = "Outro";
+
+        }
+            System.out.println(s);
+            
+
+    }
+}
