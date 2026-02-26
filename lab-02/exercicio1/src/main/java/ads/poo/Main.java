2165dade80eec951750c906eda23e36f15d13715
Giovanna <giovanna.regina2005@gmail.com>
2026-02-26 20:21:39 -0300

    exercicio 1

---
+++ lab-02/exercicio1/src/main/java/ads/poo/Main.java
@@ -0,0 +1,49 @@
+package ads.poo;
+
+
+public class Main {
+    static void main() {
+        int dia = (Integer.parseInt(("Entre com o dia: ");
+        IO.println("Dia: " + dia);
+
+        String mês = IO.readln("Entre com o mês: ");
+        IO.println("Dia: " + mês);
+
+        if (mês = dezembro && dia > 22){
+            IO.println("# Saída: Verão");
+        }
+        if (mês = março && dia < 20){
+            IO.println("# Saída: Verão");
+        }
+        if (mês = janeiro, fevereiro){
+            IO.println("# Saída: Verão");
+        }
+        if (mês = março && dia > 20){
+            IO.println("# Saída:  Outono");
+        }
+        if (mês = abril, maio){
+            IO.println("# Saída:  Outono");
+        }
+        if (mês = junho && dia < 20){
+            IO.println("# Saída:  Outono");
+        }
+        if (mês = junho && dia > 21){
+            IO.println("# Saída:  Inverno");
+        }
+        if (mês = julho, agosto){
+            IO.println("# Saída:  Inverno");
+        }
+        if (mês = setembro && dia < 22){
+            IO.println("# Saída:  Inverno");
+        }
+        if (mês = setembro && dia > 23){
+            IO.println("# Saída:  Primavera");
+        }
+        if (mês = outubro, novembro){
+            IO.println("# Saída:  Primavera");
+        }
+        if (mês = dezembro && dia < 21){
+            IO.println("# Saída:  Primavera");
+        }
+    }
+}
